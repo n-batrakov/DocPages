98247505acb06d1b366e119d48bb0293e35229ae
Xeni Kopa <xeni.kopa@gmail.com>
2018-02-09 16:41:35 +0300

    fixed gitignore for yarn, change package json, add styled components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import glamor from 'glamorous';
+
+//styled parts
+const MenuBlock = glamor.div({
+    'display': 'flex',
+    'alignItems': 'center',
+    'margin': '0 auto 12px auto',
+    'backgroundColor': 'pink',
+});
+
+const MenuList = glamor.ul({
+    'listStyle': 'none',
+    'display': 'flex',
+})
+
+const MenuItem = glamor.li({
+    'margin': '0 1em',
+})
+
 
 export default ({items}) => (
-    <div>
-        <ul>{items.map(x => <li>{x}</li>)}</ul>
-    </div>
+    <MenuBlock>
+        <MenuList>
+            {
+                items.map(x => 
+                    <MenuItem> {x} </MenuItem>
+                )
+            }
+        </MenuList>
+    </MenuBlock>
 )
