0bd35b0be04305e45d8d1c56d8c7e1e970eb5b29
Никита Батраков <n.batrakov@itexpert.ru>
2018-02-09 15:59:41 +0300

    Remove markdown formattings usage

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -4,7 +4,6 @@ export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <div>
-      <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   );
@@ -14,9 +13,6 @@ export const query = graphql`
   query DocEntryQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
-      frontmatter {
-        title
-      }
     }
   }
 `;
